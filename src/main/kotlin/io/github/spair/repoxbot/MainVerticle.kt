85aeb7ede266bd8a276e49de66a040ea61b7f79c
SpaiR <klimbetoo@gmail.com>
2018-11-05 23:03:41 +0300

    initial commit

---
+++ src/main/kotlin/io/github/spair/repoxbot/MainVerticle.kt
@@ -0,0 +1,23 @@
+package io.github.spair.repoxbot
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.logging.LoggerFactory
+
+class MainVerticle : AbstractVerticle() {
+
+    private val logger = LoggerFactory.getLogger(MainVerticle::class.java)
+
+    override fun start(startFuture: Future<Void>) {
+        vertx.createHttpServer().requestHandler { req ->
+            req.response().putHeader("content-type", "text/plain").end("Hello from Vert.x!")
+        }.listen(3000) { http ->
+            if (http.succeeded()) {
+                startFuture.complete()
+                logger.info("HTTP server started on port ${http.result().actualPort()}")
+            } else {
+                startFuture.fail(http.cause())
+            }
+        }
+    }
+}
