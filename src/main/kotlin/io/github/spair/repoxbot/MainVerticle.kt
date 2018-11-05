69a9707cc9a84751a318e4d2ea66a46570b0eb8a
SpaiR <klimbetoo@gmail.com>
2018-11-05 23:20:57 +0300

    logging preparation

--- src/main/kotlin/io/github/spair/repoxbot/MainVerticle.kt
+++ src/main/kotlin/io/github/spair/repoxbot/MainVerticle.kt
@@ -3,10 +3,15 @@ package io.github.spair.repoxbot
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.logging.LoggerFactory
+import java.io.File
 
 class MainVerticle : AbstractVerticle() {
 
     private val logger = LoggerFactory.getLogger(MainVerticle::class.java)
+
+    init {
+        File("logs").mkdir()
+    }
 
     override fun start(startFuture: Future<Void>) {
         vertx.createHttpServer().requestHandler { req ->
