d817106bfaff75fe1f9d0e16edc12d47987d4b99
SpaiR <klimbetoo@gmail.com>
2018-11-29 22:59:24 +0300

    fix ktlint

--- src/main/kotlin/io/github/spair/repoxbot/logic/generate_changelog.kt
+++ src/main/kotlin/io/github/spair/repoxbot/logic/generate_changelog.kt
@@ -14,7 +14,7 @@ fun generateChangelog(pullRequest: PullRequest): Changelog? {
 private fun findChangelogText(pullRequestBody: String): String? {
     return clText.toPattern().matcher(pullRequestBody.sanitize()).let {
         if (it.find()) {
-            (it.group(1) ?: it.group(2))!!  // 1 - :cl:, 2 - 🆑
+            (it.group(1) ?: it.group(2))!! // 1 - :cl:, 2 - 🆑
         } else {
             null
         }
